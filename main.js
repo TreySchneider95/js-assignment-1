654718688de740608b667ce78f5de57da52a8514
TreySchneider95 <tschneid95@gmail.com>
2023-09-25 11:55:52 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log('Hello World')
+console.log('============================================')
+console.log('Trey Schneider - Blue')
+console.log('============================================')
+console.log(`2 + 2 = ${2+2}`)
+console.log('============================================')
+console.log('Trey' + ' Schneider')
+console.log('============================================')
+for(let x=1;x<=10;x++){
+    console.log(x)
+}
+console.log('============================================')
+for(let x=0;x<5;x++){
+    console.log('Skillspire')
+}
+console.log('============================================')
+for(let x=1;x<11;x++){
+    console.log(x * 2)
+}
+console.log('============================================')
+for(let x=1;x<21;x++){
+    if(x%2 === 0){
+        console.log(x)
+    }
+}
+console.log('============================================')
+for(let x=1;x<21;x++){
+    if(x%2 !== 0){
+        console.log(x)
+    }
+}
+console.log('============================================')
+for(let x=1;x<11;x++){
+    if(x%2 === 0){
+        console.log('Fizz')
+    }else{
+        console.log('Buzz')
+    }
+}
